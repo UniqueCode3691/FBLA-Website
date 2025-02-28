fe6a08d730732552e60eb8666df1409eac862d56
Sam Mergu <samanyu3691@gmail.com>
2025-02-27 21:19:48 -0500

    Home page first section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import Header from './components/Header.jsx';
 import gymImage from "../assets/gym.png"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
         <>
             <Header />
             <div className="flex">
-                <div className="w-full h-90 bg-cover" style={{ backgroundImage: `url(${gymImage})` }}>
-                    <div className="relative top-0 right-0 inset-0 w-full h-90 bg-black opacity-50"></div>
-                    <h1 className="relative top-0 right-0 w-full h-90 text-5xl -mt-70 text-center text-mapletan">THE ARENA</h1>
-                    <p></p>
+                <div className="w-full h-90 bg-cover flex flex-col items-center" style={{ backgroundImage: `url(${gymImage})` }}>
+                    <div className="relative top-0 right-0 inset-0 w-full h-90 bg-black opacity-60"></div>
+                    <h1 className="relative top-0 right-0 h-90 text-5xl -mt-80 text-center font-bold text-maplegreen">MRHS ARENA</h1>
+                    <p className="relative top-0 right-0 -mt-60 text-center text-xl text-white ">Boasting 3,500 seats, Maple Ridge Arena is the perfect venue for sports, shows, and more. <span className="font-bold">Book a reservation for your event today!</span></p>
+                    <Link className="relative top-0 right-0 mt-1 sm:mt-15 py-1 px-3 bg-maplegreen rounded-xl border-4 border-mapleorange hover:cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-110 text-white text-xl" to="/rentals">RENTALS & DATES</Link>
                 </div>
             </div>
         </>
--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -4,34 +4,46 @@ import menuImage from "../../assets/menu.svg"
 import { Link } from "react-router-dom"
 import MobileNav from './MobileNavbar'
 
-export default function Header(props) {
+export default function Header() {
     
     const [isOpen, setIsOpen] = React.useState(false)
+    const [isSpinning, setIsSpinning] = React.useState(false)
 
     function toggleNavbar() {
         setIsOpen(prevIsOpen => !prevIsOpen)
+        setIsSpinning(true);
+        setTimeout(() => {
+            setIsSpinning(false);
+        }, 500);
     }
 
     console.log(isOpen)
 
     return (
         <nav className="relative top-0 left-0">
-            <div className="flex align-items-center bg-maplegreen">
-                <Link className="w-36" to="/home">
+            <div className="flex align-items-center bg-black">
+                <Link className="w-36 m-1" to="/home">
                     <img src={logoImage} alt="Maple Ridge High School Logo" />
                 </Link>
-                <h1 className="w-full justify-center text-center mt-1 text-[1.2rem] sm:m-0 sm:text-[4rem] p-6 text-white truncate">MAPLE RIDGE <span className="hidden sm:inline">BUCKS</span></h1>
-                <button onClick={toggleNavbar} className="cursor-pointer sm:hidden w-15">
+                <h1 className="w-full justify-center text-left mt-1 text-[1.2rem] sm:m-0 sm:text-[4rem] p-6 font-bold text-white truncate">MAPLE RIDGE <span className="hidden sm:inline">BUCKS</span></h1>
+                <button onClick={toggleNavbar} className={`cursor-pointer sm:hidden w-15 ${isSpinning ? 'animate-spin' : ''} transition-transform duration-1000 ease-in-out`}>
                     <img src={menuImage} alt="Menu Icon" />
                 </button>
             </div>
-            {isOpen && <MobileNav />}
+            <div className={`absolute right-0 w-full opacity-100 z-50 transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div className="flex flex-col justify-end ml-auto sm:hidden justify-items-center items-center bg-mapletan w-8/10">
+                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
+                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
+                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
+                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
+                </div>
+            </div>
 
             <div className="hidden sm:flex justify-around justify-items-center items-center bg-mapletan w-full h-20">
-                <Link className="peer/bo text-lg min-w-30 font-bold hover:text-slate-600 active:underline" to="/box-office">BOX OFFICE</Link>
-                <Link className="peer/r text-lg min-w-30 font-bold hover:text-slate-600 active:underline" to="/rentals">RENTALS</Link>
-                <Link className="peer/a text-lg min-w-30 font-bold hover:text-slate-600 active:underline" to="/athletics">ATHLETICS</Link>
-                <Link className="peer/au text-lg min-w-30 font-bold hover:text-slate-600 active:underline" to="/about-us">ABOUT US</Link>
+                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
+                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
+                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
+                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
             </div>
         </nav>
     )
