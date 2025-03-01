33a2b390918096db31a81a447215ca299a4bb39d
Sam Mergu <samanyu3691@gmail.com>
2025-02-28 20:35:08 -0500

    Home Page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import ballImage from '../assets/ball.jpg'
 import gymImage from "../assets/gym.png"
+import gameImage from "../assets/game.webp"
 import { Link } from "react-router-dom"
 
 export default function Home() {
@@ -10,10 +13,27 @@ export default function Home() {
                 <div className="w-full h-90 bg-cover flex flex-col items-center" style={{ backgroundImage: `url(${gymImage})` }}>
                     <div className="relative top-0 right-0 inset-0 w-full h-90 bg-black opacity-60"></div>
                     <h1 className="relative top-0 right-0 h-90 text-5xl -mt-80 text-center font-bold text-maplegreen">MRHS ARENA</h1>
-                    <p className="relative top-0 right-0 -mt-60 text-center text-xl text-white ">Boasting 3,500 seats, Maple Ridge Arena is the perfect venue for sports, shows, and more. <span className="font-bold">Book a reservation for your event today!</span></p>
+                    <p className="relative top-0 right-0 -mt-60 sm:mx-15 text-center text-xl text-white ">Boasting 3,500 seats, Maple Ridge Arena is the perfect venue for sports, shows, and more. <span className="font-bold">Book a reservation for your event today!</span></p>
                     <Link className="relative top-0 right-0 mt-1 sm:mt-15 py-1 px-3 bg-maplegreen rounded-xl border-4 border-mapleorange hover:cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-110 text-white text-xl" to="/rentals">RENTALS & DATES</Link>
                 </div>
             </div>
+            <div className="flex bg-amber-100">
+                <div className="w-full flex flex-col items-center">
+                    <h1 className="mt-10 sm:mt-25 text-5xl text-mapleorange font-bold">EVENTS</h1>
+                    <p className="mt-5 mx-5 text-center text-xl sm:mx-15">Being home to the Maple Ridge Bucks, this stadium is bound for greatness, with or without the ball.<span className="font-bold"> Witness it for yourself by getting a ticket to one of our events!</span></p>
+                    <Link className="text-maplered text-xl mt-5 sm:mt-15 py-1 px-3 bg-mapletan rounded-xl border-4 border-mapleorange hover:cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-110" to="/box-office">BOOK A TICKET</Link>
+                </div>
+                <img src={ballImage} alt="Ball Image" className="hidden sm:block h-8/10 w-8/10 my-10 mr-10 rounded-2xl"/>
+            </div>
+            <div className="flex bg-maplebrown">  
+                <img src={gameImage} alt="Ball Image" className="hidden sm:block h-8/10 w-8/10 my-10 ml-10 rounded-2xl"/>
+                <div className="w-full flex flex-col items-center">
+                    <h1 className="mt-10 sm:mt-25 text-5xl text-black font-bold">ATHLETICS</h1>
+                    <p className="mt-5 mx-5 text-center text-xl sm:mx-15">Our Bucks dominate the court in a multitude of sports such as basketball and volleyball. <span className="font-bold">Learn more about them.</span></p>
+                    <Link className="text-white text-xl mt-5 sm:mt-15 py-1 px-3 bg-maplegreen rounded-xl border-4 border-mapleorange hover:cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-110" to="/box-office">LEARN MORE</Link>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logoImage from "../../assets/mapleridge.png"
 import menuImage from "../../assets/menu.svg"
 import { Link } from "react-router-dom"
-import MobileNav from './MobileNavbar'
 
 export default function Header() {
     
@@ -20,31 +19,33 @@ export default function Header() {
     console.log(isOpen)
 
     return (
-        <nav className="relative top-0 left-0">
-            <div className="flex align-items-center bg-black">
+        <>
+            <div className="sticky top-0 z-100 flex align-items-center bg-black">
                 <Link className="w-36 m-1" to="/home">
-                    <img src={logoImage} alt="Maple Ridge High School Logo" />
+                    <img src={logoImage} alt="Maple Ridge High School Logo" className="transform transition-transform duration-300 ease-in-out hover:scale-110" />
                 </Link>
                 <h1 className="w-full justify-center text-left mt-1 text-[1.2rem] sm:m-0 sm:text-[4rem] p-6 font-bold text-white truncate">MAPLE RIDGE <span className="hidden sm:inline">BUCKS</span></h1>
                 <button onClick={toggleNavbar} className={`cursor-pointer sm:hidden w-15 ${isSpinning ? 'animate-spin' : ''} transition-transform duration-1000 ease-in-out`}>
                     <img src={menuImage} alt="Menu Icon" />
                 </button>
-            </div>
-            <div className={`absolute right-0 w-full opacity-100 z-50 transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-                <div className="flex flex-col justify-end ml-auto sm:hidden justify-items-center items-center bg-mapletan w-8/10">
-                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
-                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
-                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
-                    <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
+                <div className={`absolute right-0 top-20 w-full opacity-100 z-50 transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                    <div className="flex flex-col justify-end ml-auto sm:hidden justify-items-center items-center bg-mapletan w-8/10">
+                        <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
+                        <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
+                        <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
+                        <Link className="p-5 text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
+                    </div>
                 </div>
             </div>
+            <nav className="flex top-0 left-0">
 
-            <div className="hidden sm:flex justify-around justify-items-center items-center bg-mapletan w-full h-20">
-                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
-                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
-                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
-                <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
-            </div>
-        </nav>
+                <div className="hidden sm:flex justify-around justify-items-center items-center bg-mapletan w-full h-20">
+                    <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/box-office">BOX OFFICE</Link>
+                    <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/rentals">RENTALS</Link>
+                    <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/athletics">ATHLETICS</Link>
+                    <Link className="text-lg font-bold hover:text-slate-600 active:underline transition-transform duration-300 ease-in-out transform hover:scale-110 active:scale-110" to="/about-us">ABOUT US</Link>
+                </div>
+            </nav>
+        </>
     )
 }
