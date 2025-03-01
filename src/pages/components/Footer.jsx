63ffbd7af4fd41919f8eca91be2672024a9842d6
Sam Mergu <samanyu3691@gmail.com>
2025-02-28 20:35:39 -0500

    Homne page

---
+++ src/pages/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function() {
+    return (
+        <footer className="h-20 w-full bg-maplegreen flex flex-col items-center justify-center">
+            <p className="text-center text-white">Copyright © 2025 Maple Ridge High School. All Rights Reserved</p>
+        </footer>
+    )
+}
